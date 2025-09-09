3292ca5c10af45f462ea257430071e542f04e2f9
Sidhanth Pandey <sidhanthpandey@192.168.1.3>
2025-09-09 20:25:11 +0530

    setup

---
+++ pingback/pingbackApp.swift
@@ -0,0 +1,17 @@
+//
+//  pingbackApp.swift
+//  pingback
+//
+//  Created by Sidhanth Pandey on 09/09/25.
+//
+
+import SwiftUI
+
+@main
+struct pingbackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ pingbackTests/pingbackTests.swift
@@ -0,0 +1,17 @@
+//
+//  pingbackTests.swift
+//  pingbackTests
+//
+//  Created by Sidhanth Pandey on 09/09/25.
+//
+
+import Testing
+@testable import pingback
+
+struct pingbackTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
